52c6e23d22344f8d1c08233e291f963c9ba07b64
Nick Zambo <nzambo@splunk.com>
2019-11-24 15:55:13 -0500

    starting on players

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 var i = [];
+var players = [];
 
+var allowPlayerJoin = false;
 var segments = 0; //0 means 1 action per round (or D&D type). > 0 = clock type
 var currentRound = 0;
 var currentSegment = 0;
 
+
+function resetPlayers() {
+	players = [];
+}
+
+function addPlayer(playerName, charName, ipAddress, color, isGameMaster) {
+	var addPlayer = true;
+	var player = {};
+	for(p in players) {
+		if(p.playerName == playerName) {
+			addPlayer = false;
+		}
+	}
+	if(addPlayer) {
+		player = {"playerName": playerName, "charName": charName, "ipAddress": ipAddress, "color": color, "isGM": isGameMaster};
+		players.push(player);
+	}
+	return player;
+} 
+
 function newInit(s) {
 	segments = s;
 	currentRound = 1;
-	current
+        allowPlayerJoin = true;
+		
 }
 
-console.log(i);
+console.log("init system");
+newInit(0);
 
-console.log(i.length);
+console.log("adding player nick");
+var r = addPlayer("nick", "turzol", "1.2.3.4", "#ccffaa", false);
+
+console.log(players);
+
+
+
 
