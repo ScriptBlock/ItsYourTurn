e6b51cac7d0373f4b39025eefba92521d62013e0
root <root@ip-172-31-7-167.us-west-1.compute.internal>
2019-11-24 20:28:44 +0000

    testing

---
+++ index.js
@@ -0,0 +1,16 @@
+var i = [];
+
+var segments = 0; //0 means 1 action per round (or D&D type). > 0 = clock type
+var currentRound = 0;
+var currentSegment = 0;
+
+function newInit(s) {
+	segments = s;
+	currentRound = 1;
+	current
+}
+
+console.log(i);
+
+console.log(i.length);
+
