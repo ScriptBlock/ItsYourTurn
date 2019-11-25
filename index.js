a671edfec88c0cab3ed51cec29e4ce58bff20029
Nick Zambo <nzambo@splunk.com>
2019-11-24 21:36:33 -0500

    explicit setting of initiative

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 var i = [];
+/*
+	[0] = [
+
+		[0] = ]
+			{charName, topic}
+		]
+
+	]
+
+*/
+
+
 var players = [];
 
 var allowPlayerJoin = false;
@@ -26,21 +38,76 @@ function addPlayer(playerName, charName, ipAddress, color, isGameMaster) {
 	return player;
 } 
 
+function addInitItem(r, s, t) {
+	if(i[r] == null) {
+		i[r] = [];
+	}
+
+	if(i[r][s] == null) {
+		i[r][s] = [];
+	}
+
+	console.log("pushing into i[" + r + "][" + s + "]");
+	//console.log(t);
+	i[r][s].push(t);
+}
+
+function setInit(charName,segment,roundEnter) {
+	var setValid = true;
+	for(r in i) {
+		for(s in r) {
+			for(t in s) {
+				if(t.charName == charName) {
+					setValid = false;	
+				}
+			}
+		}
+	}
+
+	if(setValid) {
+		console.log("Add Init Item: " + roundEnter + " : " + segment);
+		addInitItem(roundEnter, segment, {"charName": charName, "topic": "First Action"});
+	}
+}
+
 function newInit(s) {
 	segments = s;
-	currentRound = 1;
-        allowPlayerJoin = true;
-		
+	currentRound = 0;
+    allowPlayerJoin = true;
+    i = [];
+	
 }
+
+function printInit() {
+	for(j = 0; j < i.length; j++) {
+		console.log("Round " + j)
+		if(i[j] != null) {
+			for(k = 0; k < i[j].length; k++) {
+				console.log("Segment " + k);
+				//console.log(i[j][k]);
+				if(i[j][k]) {
+					for(l = 0;l<i[j][k].length;l++) {
+						console.log(i[j][k][l]);
+					}
+				}
+			}
+		}
+	}
+}
+
 
 console.log("init system");
 newInit(0);
 
-console.log("adding player nick");
+console.log("adding players");
 var r = addPlayer("nick", "turzol", "1.2.3.4", "#ccffaa", false);
+r = addPlayer("sue", "susan", "4.3.2.1", "#cfacfa", true);
+r = addPlayer("ken", "rakion", "1.1.1.1", "#bbbbbb", true);
 
-console.log(players);
+//console.log(players);
+
+setInit("turzol", 0, currentRound);
+setInit("rakion", 5, 3);
 
 
-
-
+printInit();
